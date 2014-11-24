61f13a8b2471664a42682923f62869f8bd8df7eb
kalifou <krb.traore@etudiant.univ-lille1.fr>
2014-11-24 15:41:47 +0100

    commit EnsembleDeuxCellules

---
+++ src/chomsky/cellule/EnsembleDeCellules.java
@@ -0,0 +1,14 @@
+package chomsky.cellule;
+
+public class EnsembleDeCellules {
+
+	private Cellule[][] tableauDeuxCellules;
+
+	public EnsembleDeCellules(int l, int i){
+		tableauDeuxCellules = new Cellule[l][i];
+			
+	}
+	add(int l, int i, NonTerminal n){
+		tableauxDeuxCellules[l][i]= 		
+	}
+}

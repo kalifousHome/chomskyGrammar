df077da8c00ad5e98e4ab522595739c1e975b783
kalifou <krb.traore@etudiant.univ-lille1.fr>
2014-11-25 13:35:53 +0100

    Ensemble de Cellules fin - commit

--- src/chomsky/cellule/EnsembleDeCellules.java
+++ src/chomsky/cellule/EnsembleDeCellules.java
@@ -1,5 +1,7 @@
 package chomsky.cellule;
 
+import chomsky.composantes.NonTerminal;
+
 public class EnsembleDeCellules {
 
 	private Cellule[][] tableauDeuxCellules;
@@ -7,5 +9,15 @@ public class EnsembleDeCellules {
 	public EnsembleDeCellules(int l, int i){
 		tableauDeuxCellules = new Cellule[l][i];
 			
+	}
+	public void add(int l ,int i, NonTerminal n){
+	
+		 tableauDeuxCellules[l][i]=new Cellule();
+		 tableauDeuxCellules[l][i].add(n);
+		
+	}
+	public boolean contains(int l, int i,NonTerminal n)
+	{
+		return   tableauDeuxCellules[l][i].contains(n);
 	}
 }
